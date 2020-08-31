c1ac06e668dd6fcb75f2200c4e57ca136b732ba1
Grant <gmontgomery1994@gmail.com>
2020-08-31 12:26:14 -0700

    page testing and setup.

---
+++ pages/_document.tsx
@@ -0,0 +1,12 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body></body>
+      </Html>
+    );
+  }
+}
