1b9bbe712115b54e1f7348051586b15f3237746f
Grant <gmontgomery1994@gmail.com>
2020-09-02 10:55:38 -0700

    solutions made for links and parts. working on search box.

--- components/DateParts/DateParts.tsx
+++ components/DateParts/DateParts.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import css from "./DateParts.module.scss";
 export const DateParts: React.FC<{ location: string }> = ({ location }) => {
-  return <div className={css.dateParts}></div>;
+  return (
+    <div className={css.dateParts}>
+      <div className={css.partsWrapper}></div>
+      <div className={css.modal}></div>
+    </div>
+  );
 };
