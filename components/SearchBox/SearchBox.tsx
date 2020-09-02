0ecf416b1ac4a69c0f17edae770bcbce6cec87d8
Grant <gmontgomery1994@gmail.com>
2020-09-02 13:38:03 -0700

    hightlighting of selectors on hover.

--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
+import { SearchSelector } from "./Parts";
 import css from "./SearchBox.module.scss";
 
 export const SearchBox: React.FC = () => {
-  return <div className={css.searchBox}></div>;
+  return (
+    <div className={css.searchBox}>
+      <SearchSelector></SearchSelector>
+    </div>
+  );
 };
