525195d4fd56c947f42b1fdebd5ce3b391c5b229
Grant <gmontgomery1994@gmail.com>
2020-09-05 13:56:58 -0700

    attempting second deploy.

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Layout } from "../components";
 export default function Search() {
+  console.log("attempting another deploy.");
   return (
     <React.Fragment>
       <Layout></Layout>
